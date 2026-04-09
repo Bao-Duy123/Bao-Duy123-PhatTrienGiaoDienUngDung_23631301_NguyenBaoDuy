4c17384dba33e340aef9bdcd2141b8ab2444ab64
Nguyen Bao Duy <nbduy99@gmail.com>
2026-04-09 07:22:57 +0700

    commit bai 1 Counter Global + minh chung

---
+++ Tuan7_6-4_State/recoil/src/App.jsx
@@ -0,0 +1,15 @@
+import ComponentA from './components/ComponentA';
+import ComponentB from './components/ComponentB';
+
+function App() {
+  return (
+    <div >
+      <h1>Bài 1: Counter Global State</h1>
+      
+      <ComponentA />
+      <ComponentB />
+    </div>
+  );
+}
+
+export default App;
